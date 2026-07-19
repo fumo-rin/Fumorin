924fb876e10a449bb00e553ca1fddb7f2820168f
fumo rin <fumorinw@gmail.com>
2026-07-19 03:29:51 +0200

    gawns

--- Scripts/Fumo Engine 1/Music Player/StageMusic.cs
+++ Scripts/Fumo Engine 1/Music Player/StageMusic.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 namespace rinCore
 {
-    public class StageMusic : MonoBehaviour
+    public class StageMusic : MonoBehaviour, IHierarchyComponentColor
     {
         [SerializeField] MusicWrapper music;
+        public Color LabelColor => ColorHelper.PastelYellow.Opacity(50);
+
         private void Start()
         {
             music.Play();
